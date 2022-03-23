849b848129f85b953aa6ac4bccf3949ed92cbb58
Luther Nogueira da Silva <luthernogueira@gmail.com>
2022-03-23 10:45:51 -0300

    criada estrutura classe api

---
+++ src/api/Api.js
@@ -0,0 +1,14 @@
+const Api = class Api{
+
+    static async request(){
+  
+      let objectsArray = [{}]
+  
+      let response = await fetch('')
+      let data = await response.json();
+  
+      
+  }
+}
+  
+  export {Api}
