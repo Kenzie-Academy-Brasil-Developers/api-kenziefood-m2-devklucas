9754c62acee2c3cfbd67736ed5bf006119084dc3
Luther Nogueira da Silva <luthernogueira@gmail.com>
2022-03-23 11:31:00 -0300

    adicionado construtor produto

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,18 +1,18 @@
 const Api = class Api{
 
-    static async requestProducts(){
+    static async requestProdutos(){
   
-      let productsArray = [{}]
+      let arrayProdutos = [{}]
   
       let response = await fetch('https://kenzie-food-api.herokuapp.com/products')
       let data = await response.json();
 
       for(let i = 0; i<data.length;i++){
-        productsArray[i] = data[i]
+        arrayProdutos[i] = data[i]
       }
-      return productsArray      
+      return arrayProdutos      
   }
-  
+
 }
   
   export {Api}
--- src/models/produto.js
+++ src/models/produto.js
@@ -1,3 +1,23 @@
 const Produto = class Produto{
-    
+    constructor(nome,preco,descricao,imagem,createdAt,updatedAt){
+        this._nome = nome
+        this._preco = preco
+        this._descricao = descricao
+        this._urlImagem = imagem
+        this._dataCriacao = createdAt
+        this._dataAtualizacao = updatedAt
+    }
 }
+
+/* 
+Estrutura json produtos
+
+{"id":1,
+"nome":"Mousse de morango com a fruta",
+"preco":27.5,"categoria":"Frutas",
+"descricao":"Sobremesa fácil, rápida e muito saborosa: a mousse de morango leva apenas 5 ingredientes; confira como fazer a receita",
+"imagem":"https://kenzie-academy-brasil.gitlab.io/fullstack/frontend/modulo2/sprint4/img/capstone-images/mousse.png",
+"createdAt":"2022-03-19T16:16:00.629Z",
+"updatedAt":"2022-03-19T16:16:00.629Z"} 
+
+*/
