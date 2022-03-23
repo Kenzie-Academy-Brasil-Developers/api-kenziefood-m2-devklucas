57a4fa78fdf44d7798393bc16855ed4701ea2024
Luther Nogueira da Silva <luthernogueira@gmail.com>
2022-03-23 11:07:03 -0300

    adicionada classe api para coleta dos  produtos

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,14 +1,18 @@
 const Api = class Api{
 
-    static async request(){
+    static async requestProducts(){
   
-      let objectsArray = [{}]
+      let productsArray = [{}]
   
-      let response = await fetch('')
+      let response = await fetch('https://kenzie-food-api.herokuapp.com/products')
       let data = await response.json();
-  
-      
+
+      for(let i = 0; i<data.length;i++){
+        productsArray[i] = data[i]
+      }
+      return productsArray      
   }
+  
 }
   
   export {Api}
