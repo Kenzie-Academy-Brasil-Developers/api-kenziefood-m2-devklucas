243faf2e60802568514f55cea9e051e85c06f6f5
Luther Nogueira da Silva <luthernogueira@gmail.com>
2022-03-23 14:39:42 -0300

    layout e listagem vitrine DOM

--- src/models/produto.js
+++ src/models/produto.js
@@ -1,24 +1,12 @@
 const Produto = class Produto{
-    constructor(id,nome,preco,descricao,imagem,createdAt,updatedAt){
+    constructor(id,nome,preco,categoria,descricao,imagem,createdAt,updatedAt){
         this._id = id
         this._nome = nome
         this._preco = preco
+        this._categoria = categoria
         this._descricao = descricao
         this._urlImagem = imagem
         this._dataCriacao = createdAt
         this._dataAtualizacao = updatedAt
     }
 }
-
-/* 
-Estrutura json produtos
-
-{"id":1,
-"nome":"Mousse de morango com a fruta",
-"preco":27.5,"categoria":"Frutas",
-"descricao":"Sobremesa fácil, rápida e muito saborosa: a mousse de morango leva apenas 5 ingredientes; confira como fazer a receita",
-"imagem":"https://kenzie-academy-brasil.gitlab.io/fullstack/frontend/modulo2/sprint4/img/capstone-images/mousse.png",
-"createdAt":"2022-03-19T16:16:00.629Z",
-"updatedAt":"2022-03-19T16:16:00.629Z"} 
-
-*/
--- src/models/vitrine-produtos.js
+++ src/models/vitrine-produtos.js
@@ -6,7 +6,7 @@ const VitrineProdutos = class VitrineProdutos {
         const vitrinePrincipal = document.querySelector(".listaProdutos")
 
         vitrinePrincipal.innerHTML = ""
-        
+
         arrayProdutos.forEach((item)=>{
           const li = this.templateVitrine(item)
           vitrinePrincipal.appendChild(li)
@@ -14,7 +14,7 @@ const VitrineProdutos = class VitrineProdutos {
         })
       }
     
-      static templateVitrine({id,nome,preco,descricao,imagem}){
+      static templateVitrine({id,nome,preco,categoria,descricao,imagem}){
         const li  = document.createElement("li")
         li.classList.add("containerCardProduto")
 
@@ -23,11 +23,12 @@ const VitrineProdutos = class VitrineProdutos {
         <img src="${imagem}" alt="${nome}">
         </div>
         <div class="containerDetalhesProduto">
-            <h1>${nome}</h1>
-            <caption>${descricao}</caption>
+            <h1 class="marginTopBottom10px">${nome}</h1>
+            <p class="marginTopBottom10px">${descricao}</p>
+            <p class="marginTopBottom10px">${categoria}</p>
             <div class="footerCarrinho">
-                <h2>${preco}</h2>
-                <button id="${id}" class="btnCarrinho">
+                <h2 class="marginTopBottom10px">${preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</h2>
+                <button id="${id}" class="btnCarrinho marginTopBottom10px">
                     <i class="fa-solid fa-cart-plus"></i>
                 </button>
             </div>
